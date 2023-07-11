42a639a5fdd149dacb9c91bb36ab1c59e00227e2
Redshore <isutare412@gmail.com>
2023-07-11 13:18:23 +0900

    chore: add custom daisyUI theme

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -7,6 +7,20 @@ module.exports = {
   ],
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: ['light'],
+    themes: [
+      {
+        mytheme: {
+          primary: '#fb923c',
+          secondary: '#fcd34d',
+          accent: '#a3e635',
+          neutral: '#1f2937',
+          'base-100': '#ffffff',
+          info: '#3abff8',
+          success: '#36d399',
+          warning: '#fbbd23',
+          error: '#f87272',
+        },
+      },
+    ],
   },
 }

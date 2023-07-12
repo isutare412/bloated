c875fcb453c89c307f40e4ed5021953db0c01edb
Redshore <isutare412@gmail.com>
2023-07-13 00:11:27 +0900

    docs: explain bloated UI

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -5,21 +5,16 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  safelist: [
-    'alert-info',
-    'alert-success',
-    'alert-warning',
-    'alert-error',
-  ],
+  safelist: ['alert-info', 'alert-success', 'alert-warning', 'alert-error'],
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
     themes: [
       {
-        mytheme: {
-          primary: '#fb923c',
-          secondary: '#fcd34d',
-          accent: '#a3e635',
-          neutral: '#1f2937',
+        light: {
+          primary: '#ea580c',
+          secondary: '#db2777',
+          accent: '#65a30d',
+          neutral: '#2b3440',
           'base-100': '#ffffff',
           info: '#3abff8',
           success: '#36d399',

2dba85a81d827db0e34fd875c4dc09f616bb1c06
Redshore <isutare412@gmail.com>
2023-07-24 10:12:00 +0900

    feat: show loading dots in pokemon list page

--- ui/src/routes/pokemons/+page.svelte
+++ ui/src/routes/pokemons/+page.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
+	import { navigating } from '$app/stores'
+	import LoadingDots from '$components/LoadingDots.svelte'
 	import PageTitle from '$components/text/PageTitle.svelte'
 	import type { PageData } from './$types'
 
 	export let data: PageData
 </script>
 
+{#if $navigating}
+	<LoadingDots />
+{:else}
 <PageTitle title="Pokemons" />
 <ul class="list-inside list-disc space-y-1">
 	{#each data.defaultPokemons as pokemon}
 		<li><a href={`/pokemons/${pokemon.id}`} class="link">{pokemon.name}</a></li>
 	{/each}
 </ul>
+{/if}
